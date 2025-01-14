a4e3771962d6d529b1182abb182ae80d3e2183fb
Pekka <pekkaylajussila@gmail.com>
2025-01-14 15:33:21 +0200

    Edit main.py fuction call circle radius

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ GetLogDataTime()
 GetLogTimestamp()
 
 # Calculate area of circle
-<<<<<<< HEAD
+
 # CalcLogAreaOfCircle(20)
-=======
 CalcLogAreaOfCircle(20)
-CalcLogAreaOfCircle(200)
->>>>>>> 853ffc31aab12dda58a5e106be795cc2649a60dd
+CalcLogAreaOfCircle(300)
